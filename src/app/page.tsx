66f6bfba978746b499e6930dd6985fd20479610f
Liam Graham <liam.david.graham@gmail.com>
2024-06-08 22:08:32 +1000

    Improve chat pane styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ChatPane from "../components/chat/chatPane";
 
 export default async function Home() {
   return (
-    <main className="flex flex-col h-screen w-full items-center justify-center px-52">
+    <main className="flex flex-col h-screen w-full items-center justify-center">
       <ChatPane/>
     </main>
   );
--- src/components/chat/chatPane.tsx
+++ src/components/chat/chatPane.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { type ReactNode } from "react";
 import { DynamicTextarea, ScrollArea } from "~/components/ui"
+import { faker } from '@faker-js/faker';
 
 interface ChatBubbleProps {
   className?: string;
@@ -10,9 +11,9 @@ interface ChatBubbleProps {
 
 function ChatBubble(props: ChatBubbleProps) {
   const { children, fromUser, className } = props;
-  const alignmentClass = fromUser ? 'self-end bg-blue-500 text-white max-w-[66%]' : 'self-start bg-gray-300 max-w-full text-black';
+  const senderClass = fromUser ? 'self-end max-w-[66%] bg-primary text-primary-foreground' : 'self-start bg-gray-300 max-w-full bg-transparent text-secondary-foreground';
   return (
-    <div className={`items-center rounded-lg border px-1.5 py-1 text-xs focus:outline-none border-transparent bg-primary text-primary-foreground ${alignmentClass} ${className}`}>
+    <div className={`items-center rounded-lg border px-1.5 py-1 focus:outline-none font-sm border-transparent leading-relaxed ${senderClass} ${className}`}>
       {children}
     </div>
   )
@@ -20,55 +21,16 @@ function ChatBubble(props: ChatBubbleProps) {
 
 export default function ChatPane() {
   return (
-    <div className="flex flex-col w-full h-full p-4">
-      <ScrollArea className="flex-1 py-4 space-y-4">
+    <div className="flex flex-col w-1/2 h-full p-4">
+      <ScrollArea className="flex-1 py-4">
         <div className="flex pr-3 flex-col gap-3">
-          <ChatBubble fromUser={true}>This is some text (1)</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>This is some text</ChatBubble>
-          <ChatBubble fromUser={true}>This is some text</ChatBubble>
-          <ChatBubble fromUser={false}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel neque cursus, mollis est ac, ultrices justo. In orci diam, vestibulum non bibendum et, egestas ac arcu. Curabitur aliquet eleifend ex, sed ullamcorper purus dapibus at. Phasellus convallis ac risus eu convallis. Ut nibh augue, elementum in metus a, viverra vehicula quam. Praesent sit amet tellus aliquam felis placerat placerat. Ut blandit rutrum erat nec dignissim.
-
-            Integer vel tempus velit. Pellentesque ac risus condimentum, consectetur lacus in, facilisis sem. Nulla ac ligula lacinia, pretium purus sit amet, sagittis massa. Praesent ut ligula vestibulum, aliquam nulla non, placerat arcu. Nullam sagittis hendrerit elit, sit amet ornare lectus mattis eget. Fusce eu nisi a justo interdum aliquet. Proin at orci interdum, ullamcorper sapien eget, vehicula nisl. Etiam a aliquam nibh, eu sodales odio. Quisque euismod feugiat nisl, sit amet lobortis tortor. Mauris mattis nulla dui, eget tristique elit consectetur sed. Donec eleifend feugiat nibh, non dapibus quam dapibus non. Pellentesque rutrum eget lectus rutrum iaculis. Aliquam sodales ex sit amet rutrum lobortis. Curabitur mattis malesuada orci, sit amet tincidunt ante blandit non. Etiam hendrerit velit nec ligula vestibulum, sed dignissim eros tempor. Suspendisse nec nunc lectus.
-          </ChatBubble>
+          {Array.from({ length: 20 }, (_, i) => {
+            const isUser = i % 2 == 0;
+            return <ChatBubble key={i} fromUser={isUser}>{faker.lorem.sentence({min: 20, max: isUser ? 30 : 150})}</ChatBubble>
+          })}
         </div>
       </ScrollArea>
-      <DynamicTextarea onEnterPress={() => console.log("Enter")} />
+      <DynamicTextarea className="text-md" onEnterPress={() => console.log("Enter")} />
     </div>
   )
 }
