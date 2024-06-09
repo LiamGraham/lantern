cf6bde2777840908f33802ea93dc51ea2eaa4187
Liam Graham <liam.david.graham@gmail.com>
2024-06-09 12:50:42 +1000

    Add Up env vars

--- src/env.js
+++ src/env.js
@@ -20,8 +20,10 @@ export const env = createEnv({
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
+      process.env.VERCEL ? z.string() : z.string().url(),    
     ),
+    UP_API_TOKEN: z.string(),
+    UP_API_URL: z.string().url(),
   },
 
   /**
@@ -42,6 +44,8 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    UP_API_TOKEN: process.env.UP_API_TOKEN,
+    UP_API_URL: process.env.UP_API_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
